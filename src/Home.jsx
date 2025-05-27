698b31360096db6461bdc425bd0236ca59e03a2a
Erwing Hernandez <birc89@hotmail.com>
2025-05-26 19:10:42 -0600

    actualizacion del home

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,18 @@
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { motion } from 'framer-motion'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faBookOpen,
+    faLightbulb,
+    faCode,
+    faLayerGroup,
+    faBullseye,
+    faPaperclip,
+    faClipboardList
+} from '@fortawesome/free-solid-svg-icons'
+
 import './App.css'
 
 function Home() {
@@ -33,24 +45,32 @@ function Home() {
                     {/*Contenedor de las partes */}
                     <div className='contenedor-tarjetas'>
                         <div id='documento'>
-                            <div className='card' id='Card1' onClick={() => handleCardClick("/part1")}>
-                                Introducción
+
+                            <div className='card card-styled' onClick={() => handleCardClick("/part1")}>
+                                
+                                <FontAwesomeIcon icon={faBookOpen} bounce  size="2x" className="icon-hover-bounce"/>
+                                <span>Introducción</span>
                             </div>
 
-                            <div className='card' id='Card2' onClick={() => handleCardClick("/part2")}>
-                                Marco Teórico
+                            <div className='card card-styled' onClick={() => handleCardClick("/part2")}>
+                                <FontAwesomeIcon icon={faLightbulb}  bounce  size="2x" />
+                                <span>Marco Teórico</span>
                             </div>
 
-                            <div className='card' id='Card3' onClick={() => handleCardClick("/part3")}>
-                                Desarrollo
+                            <div className='card card-styled' onClick={() => handleCardClick("/part3")}>
+                                <FontAwesomeIcon icon={faCode}  bounce  size="2x" />
+                                <span>Desarrollo</span>
                             </div>
 
-                            <div className='card' id='Card4' onClick={() => handleCardClick("/part4")}>
-                                1 part
+                            <div className='card card-styled' onClick={() => handleCardClick("/part4")}>
+                                <FontAwesomeIcon icon={faLayerGroup}  bounce  size="2x" />
+                                <span>Primera Parte</span>
                             </div>
+
 
 
                         </div>
+
 
                         <div id='accion' onClick={() => handleCardClick("/planaccion")}>
                             <div id='card5'>Plan de Acción</div>
@@ -61,16 +81,19 @@ function Home() {
 
                     {/*Contenedor del panel de la derecha */}
                     <div className='right-panel'>
-                        <div id='ob.ti' className='card' onClick={() => handleCardClick("/objetivosti")}>
-                            objetivos de ti
+                        <div className='card card-styled' onClick={() => handleCardClick("/objetivosti")}>
+                            <FontAwesomeIcon icon={faBullseye}  bounce  size="2x" />
+                            <span>Objetivos TI</span>
                         </div>
-                        <div id='anexos' className='card'>
-                            Anexos
+
+                        <div className='card card-styled'>
+                            <FontAwesomeIcon icon={faPaperclip}  bounce  size="2x" />
+                            <span>Anexos</span>
                         </div>
 
                     </div>
 
-                      {/*Contenedor del panel de la derecha */}
+                    {/*Contenedor del panel de la derecha */}
                 </div>
 
             </motion.div>
